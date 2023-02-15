d4b562547d68bc9bb30ecb69d955e1b84a1bf653
Isengrine <isengrine@gmail.com>
2023-02-15 15:05:08 -0600

    Add random profile image generation

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 function getRandomNum() {
-    return Math.floor(Math.random() * 3);
+    return Math.floor(Math.random() * 4);
+}
+
+function generatedImage() {
+    switch (imageNum) {
+        case 0:
+            image = '<img src="./images/legend_image_1.svg" id="legend_img" alt="A placeholder profile picture">';
+            break;
+        case 1:
+            image = '<img src="./images/legend_image_2.svg" id="legend_img" alt="A placeholder profile picture">';
+            break;
+        case 2:
+            image = '<img src="./images/legend_image_3.svg" id="legend_img" alt="A placeholder profile picture">';
+            break;
+        case 3:
+            image = '<img src="./images/legend_image_4.svg" id="legend_img" alt="A placeholder profile picture">';
+            break;
+    }
+    return image;
 }
 
 let imageNum = getRandomNum();
+let image = "";
+
+const legend = document.getElementById("legend");
+
+window.addEventListener("load", () => {
+    legend.innerHTML = generatedImage();
+})
