43c3254f72e8066b3e7b770484282687302c9e4f
Isengrine <isengrine@gmail.com>
2023-02-15 19:19:59 -0600

    Add simple password validation

--- script.js
+++ script.js
@@ -24,7 +24,41 @@ let imageNum = getRandomNum();
 let image = "";
 
 const legend = document.getElementById("legend");
+const submitBtn = document.getElementById("submit_button");
+let password = document.getElementById("password");
+let confirmPassword = document.getElementById("confirm_password");
+let passwordsMatch = document.getElementById("passwords_match");
 
 window.addEventListener("load", () => {
     legend.innerHTML = generatedImage();
+})
+
+password.addEventListener("keyup", () => {
+    if (password.value == confirmPassword.value) {
+        password.style.borderColor = "green";
+        confirmPassword.style.borderColor = "green";
+        passwordsMatch.innerText = "";
+        submitBtn.removeAttribute("disabled");
+    }
+    else {
+        password.style.borderColor = "red";
+        confirmPassword.style.borderColor = "red";
+        passwordsMatch.innerText = "* Passwords do not match";
+        submitBtn.setAttribute("disabled", "");
+    }
+})
+
+confirmPassword.addEventListener("keyup", () => {
+    if (confirmPassword.value == password.value) {
+        password.style.borderColor = "green";
+        confirmPassword.style.borderColor = "green";
+        passwordsMatch.innerText = "";
+        submitBtn.removeAttribute("disabled");
+    }
+    else {
+        password.style.borderColor = "red";
+        confirmPassword.style.borderColor = "red";
+        passwordsMatch.innerText = "* Passwords do not match";
+        submitBtn.setAttribute("disabled", "");
+    }
 })
